8ff697c27b2142958145eacc4666899e21c21889
crfmattos <gabrielmbserra@gmail.com>
2024-05-09 13:47:23 -0300

    sadsa

---
+++ 5Exercicio/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] nomes = new string[5];
+double[] notas = new double[5];
+
+PopularArrayNomes("Gabriel", "Maria", "Lucas", "Iana", "sla");
+
+ void PopularArrayNomes(string[] nome)
+{
+	foreach (string name in nome)
+	{
+		string n = Console.ReadLine();
+		nomes.Append(n);
+	}
+}
+
+
+double nota = Convert.ToDouble(Console.ReadLine());

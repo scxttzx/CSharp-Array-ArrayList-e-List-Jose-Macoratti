0d357cda678d26a20e02c6e6c642d7e8d3697053
crfmattos <gabrielmbserra@gmail.com>
2024-05-22 15:10:06 -0300

    Aula 22/05/2024 - 15:09

---
+++ Exercicio5/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5;
+
+public class Produto
+{
+	public string Nome { get; set; }
+	public decimal Preco { get; set; }
+
+	public static void Exibir(List<Produto> produtos)
+	{
+		foreach (var produto in produtos)
+		{
+			Console.WriteLine($"Produto: {produto.Nome} - Preço: {produto.Preco.ToString("C", CultureInfo.CurrentCulture)}");
+		}
+        Console.WriteLine($"\nO preço total do carrinho é: {Soma(produtos).ToString("C", CultureInfo.CurrentCulture)}");
+		Console.WriteLine($"\nA média de preço dos produtos somados é: {Media(produtos).ToString("C", CultureInfo.CurrentCulture)}");
+	}
+	public static decimal Soma(List<Produto> produtos)
+	{
+		decimal total = 0;
+		foreach (var produto in produtos)
+		{
+			total += produto.Preco;
+		}
+		return total;
+	}	
+	public static decimal Media(List<Produto> produtos)
+	{
+		decimal totalItens = produtos.Count();	
+		decimal media = Soma(produtos) / totalItens;
+		return media;
+	}
+
+}
+
---
+++ Exercicio5/Program.cs
@@ -0,0 +1,30 @@
+﻿using Exercicio5;
+
+List<Produto> produtos = new();
+
+Console.WriteLine("## Adicionando produtos ##\n");
+
+string continuar;
+do
+{
+    Console.Write("Informe o nome do produto: ");
+    string nome = Console.ReadLine();
+
+	Console.Write("Informe o preço do produto: R$ ");
+	decimal preco = Convert.ToDecimal(Console.ReadLine());
+
+	produtos.Add(new Produto { Nome = nome, Preco = preco });
+
+    Console.WriteLine($"\nAdicionando o item ao carrinho... \n");
+
+    Console.Write("\nDeseja adicionar mais produtos ?(S/N)");
+	continuar = Console.ReadLine();
+    Console.WriteLine();
+
+} while (continuar == "s" || continuar == "S");
+
+
+Produto.Exibir(produtos);
+
+Console.WriteLine("## Adicionando uma Mochila ##");
+produtos.Add(new Produto { Nome = "Mochila", Preco = 22.44m });

821b9e4fa553b9449750dd2e394ce0a191402939
crfmattos <gabrielmbserra@gmail.com>
2024-05-24 12:53:57 -0300

    Exercicio 24/05/2024

--- Exercicio 6/Program.cs
+++ Exercicio 6/Program.cs
@@ -1,6 +1,18 @@
-﻿List<Object> objetos = new ();
+﻿using Exercicio6;
 
-public void ProcessaObjetos()
+List<object> objetos = new ();
+
+void ProcessaObjetos(object objs)
 {
+	objetos.Add(objs);
+}
 
+ProcessaObjetos(1);
+ProcessaObjetos("Maria");
+ProcessaObjetos(new Teste());
+ProcessaObjetos(null a);
+
+foreach (var obj in objetos)
+{
+	Console.WriteLine($"\nObjeto: {obj} - Tipo: {obj.GetType()}");
 }
